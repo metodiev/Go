6924b6721f5aa7378ea41c99bf9724bfa6857544
y509199 <y509199@softwareag.com>
2022-05-04 21:42:38 +0300

    Go garbage collector

---
+++ master go/2. Understanding Go Internals/unsafe.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+
+	if len(arguments) == 1 {
+		fmt.Println("please input more than one argument")
+		os.Exit(1)
+	} else {
+		for i := 0; i < len(arguments); i++ {
+			//integerNumber
+		}
+	}
+}
