32da55fbda65bc54a77e3a2851a96df0c99d2c5f
y509199 <y509199@softwareag.com>
2022-05-19 19:02:34 +0300

    add reverse array task from hacker rank chalenge

---
+++ hackerrank-30days-ofcode/8. Arrays/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < int(n); i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+    
+        printReverseArray(0, n-1, arr)
+}
+
+func printReverseArray(l, h int32, arr [] int32){
+    if l > h {
+        return
+    } else {
+        printReverseArray(l+1, h , arr)
+        fmt.Printf("%d ", arr[l])
+
+    }
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
