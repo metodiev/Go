f1044a4efd5b6f5de034f93d5058d1c6d5b9bfbc
Miroslav Metodiev <y509199@softwareag.com>
2022-06-10 20:23:40 +0300

    add example how to use more than one  package in go

---
+++ master go/6. Packages and Functions/useAPackage.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"aPackage"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Using aPackage!")
+	aPackage.A()
+	aPackage.B()
+	fmt.Println(aPackage.MyConstant)
+}
+
+// $ mkdir ~/go/src/aPackage
+// $ cp aPackage.go ~/go/src/aPackage/
+// $ go install aPackage
+// $ cd ~/go/pkg/darwin_amd64/
+// $ ls -l aPackage.a
+// -rw-r--r-- 1 mtsouk staff 4980 Dec 22 06:12 aPackage.a
