a47b07219a1cc1090820e80dca51690c4d224b30
metodiev <miroslav.l.metodiev@gmail.com>
2022-09-17 16:38:14 +0300

    add the easy part with go examples

---
+++ GoByExample/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+	fmt.Println(b+1)
+
+    var d = true
+    fmt.Println(!d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println(f+ "t")
+}
