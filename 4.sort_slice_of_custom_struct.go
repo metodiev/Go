2884de5e6a74f73dad7e29a714cf9623424067de
metodiev <miroslav.l.metodiev@gmial.com>
2025-01-22 23:53:25 +0200

    add inteview tasks

--- 4.sort_slice_of_custom_struct.go
+++ 4.sort_slice_of_custom_struct.go
@@ -15,3 +15,33 @@ type Human struct {
     name string
     age int
 }
+
+//ageFactor implemets sort.Interface that sorts the slice based on age filed
+
+type AgeFactor []Human
+
+func(a AgeFactor) Len int {
+    return len(a)
+}
+
+
+func(a AgeFactor) Less(i, j int) bool {
+    return a[i].age < a[j].age
+}
+
+func (a AgeFactor) Swap(i, j int) {
+    a[i], a[j] = a[j], a[i]
+}
+
+func main(){
+    audience := []Human{
+        {"Alice, 35"},
+        {"Bob", 45},
+        {"James", 25},
+    }
+
+    sort.Sort(AgeFactor(audience))
+    fmt.Println(audience)
+}
+
+
